3393e186cfc5ffc80a72e4e0436c9826952d5389
Francois Merat <Francois.Merat@telecomnancy.eu>
2023-01-03 14:29:01 +0100

    [UPDATE] sceneController

--- src/main/java/com/example/flashcards/controller/SceneController.java
+++ src/main/java/com/example/flashcards/controller/SceneController.java
@@ -1,30 +1,23 @@
 package com.example.flashcards.controller;
 
-import com.example.flashcards.models.DeckContainer;
-import com.example.flashcards.view.*;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
 import java.util.ArrayList;
 
 public class SceneController{
     private Stage stage;
-    private ArrayList<Scene> Scenes;
+    private ArrayList<Scene> scenes;
 
     public SceneController(Stage stage){
         this.stage=stage;
+        scenes = new ArrayList<>();
     }
+
     public void addScene(Scene scene){
-        this.Scenes.add(scene);
+        this.scenes.add(scene);
     }
-    @FXML
-    public void Leave(){
-        Platform.exit();
-    }
-    @FXML
-    public void changeScene(int etat){
-        ViewState.changeScene(etat);
+
+    public void changeScene(int i) {
+        stage.setScene(scenes.get(i));
     }
 }
