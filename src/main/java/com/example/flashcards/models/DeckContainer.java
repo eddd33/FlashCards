83cb8707ecaf06cc57dcb30e7475e8bbff6ca6e8
Francois Merat <Francois.Merat@telecomnancy.eu>
2023-01-03 11:51:07 +0100

    [UPDATE] DeckContainer implment SUbjectObserver

--- src/main/java/com/example/flashcards/models/DeckContainer.java
+++ src/main/java/com/example/flashcards/models/DeckContainer.java
@@ -1,18 +1,40 @@
 package com.example.flashcards.models;
 
+import com.example.flashcards.view.Observer;
+import com.example.flashcards.view.SubjectObserver;
+
 import java.util.ArrayList;
 
-public class DeckContainer {
+public class DeckContainer implements SubjectObserver {
 
     private ArrayList<Deck> decks;
     private ArrayList<Card> cards;
+    private ArrayList<Observer> listObs;
 
     public DeckContainer() {
         decks = new ArrayList<>();
         cards = new ArrayList<>();
+        listObs = new ArrayList<>();
     }
 
+    /**
+     * SubjectObserver :
+     * The following methods are used implementing the SubjectObserver interface.
+     */
 
+    @Override
+    public void addObserver(Observer o) {
+        listObs.add(o);
+    }
+
+    @Override
+    public void notifyObserver() {
+        for (Observer o : listObs) {
+            o.update();
+        }
+    }
+
+    
 
     /**
      * Getter :
