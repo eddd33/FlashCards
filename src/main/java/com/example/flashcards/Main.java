a6f36ab654621e79101520d7a9e5b8236823996c
Francois Merat <Francois.Merat@telecomnancy.eu>
2023-01-03 14:22:00 +0100

    [UPDATE] add scene construction

--- src/main/java/com/example/flashcards/Main.java
+++ src/main/java/com/example/flashcards/Main.java
@@ -1,8 +1,16 @@
 package com.example.flashcards;
 
+import com.example.flashcards.controller.CreateViewController;
+import com.example.flashcards.controller.LearningViewController;
+import com.example.flashcards.controller.SceneController;
+import com.example.flashcards.controller.SelectionViewController;
+import com.example.flashcards.models.DeckContainer;
+import com.example.flashcards.view.ViewState;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +18,47 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        SceneController controller = new SceneController(stage);
+        DeckContainer container = new DeckContainer();
+        ViewState state = new ViewState();
+
+
+        /**
+         * Deck selection view.
+         */
+        FXMLLoader selectionViewLoader = new FXMLLoader();
+        selectionViewLoader.setLocation(getClass().getResource("aaa.fxml"));
+        selectionViewLoader.setControllerFactory(iC->new SelectionViewController(container,state));
+        Parent selectionView = selectionViewLoader.load();
+        Scene selectionScene = new Scene(selectionView, 320, 240);
+        controller.addScene(selectionScene);
+
+
+        /**
+         * Deck selection view.
+         */
+        FXMLLoader learningViewLoader = new FXMLLoader();
+        learningViewLoader.setLocation(getClass().getResource("aaa.fxml"));
+        learningViewLoader.setControllerFactory(iC->new LearningViewController(container,state));
+        Parent learningView = learningViewLoader.load();
+        Scene learningScene = new Scene(learningView, 320, 240);
+        controller.addScene(learningScene);
+
+
+        /**
+         * Deck selection view.g
+         */
+        FXMLLoader createViewLoader = new FXMLLoader();
+        createViewLoader.setLocation(getClass().getResource("aaa.fxml"));
+        createViewLoader.setControllerFactory(iC->new CreateViewController(container,state));
+        Parent createView = createViewLoader.load();
+        Scene createScene = new Scene(createView, 320, 240);
+        controller.addScene(createScene);
+
+
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(learningScene); // pour l'instant.
         stage.show();
     }
 
--- src/main/java/com/example/flashcards/models/DeckContainer.java
+++ src/main/java/com/example/flashcards/models/DeckContainer.java
@@ -34,7 +34,7 @@ public class DeckContainer implements SubjectObserver {
         }
     }
 
-    
+
 
     /**
      * Getter :
