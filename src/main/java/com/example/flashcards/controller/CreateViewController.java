e1f8a0ca032412221fa2f350c445f229987d2630
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2023-01-04 09:17:58 +0100

    [add] searchByTag

--- src/main/java/com/example/flashcards/controller/CreateViewController.java
+++ src/main/java/com/example/flashcards/controller/CreateViewController.java
@@ -1,11 +1,13 @@
 package com.example.flashcards.controller;
 
 import com.example.flashcards.command.ChangeSceneCommand;
+import com.example.flashcards.models.Card;
 import com.example.flashcards.models.DeckContainer;
 import com.example.flashcards.view.*;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -59,6 +61,27 @@ public class CreateViewController implements Observer, Initializable {
         new ChangeSceneCommand(viewState,0).execute();
     }
 
-    public void searchByTag(){}
+    public void searchByTag(){
+        String searchTag = "tag"; //TODO : get the tag from the textfield
+        boolean isFound = false;
+        ListView<Card> result = new ListView<>();
+        for (Card card : app.getCards()) {
+            for (String tag : card.getTagList()) {
+                if (tag.equals(searchTag)) {
+                    isFound = true;
+                    result.getItems().add(card);
+                    break;
+                }
+            }
+            if (!isFound) {
+                System.out.println("Card not found");
+            }
+        }
+    }
+    /*
+    public void newTag(){
+        String newTag = "tag"; //TODO : get the tag from the textfield
+        card.getTagList().add(newTag);
+    }*/
 
 }
