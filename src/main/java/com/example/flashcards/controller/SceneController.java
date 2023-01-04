330f5f93a9c71bf59033bbae86457ef6a50800a0
Francois Merat <Francois.Merat@telecomnancy.eu>
2023-01-04 08:46:28 +0100

    [UPDATE] plus de nettoyage de code

--- src/main/java/com/example/flashcards/controller/SceneController.java
+++ src/main/java/com/example/flashcards/controller/SceneController.java
@@ -8,15 +8,35 @@ public class SceneController{
     private Stage stage;
     private ArrayList<Scene> scenes;
 
+    /**
+     * @param stage
+     * The main Stage for the application.
+     */
     public SceneController(Stage stage){
-        this.stage=stage;
+        this.stage = stage;
         scenes = new ArrayList<>();
     }
 
+
+
+    /**
+     * @param scene
+     * New Scene to add to the ArrayList scenes.
+     */
     public void addScene(Scene scene){
         this.scenes.add(scene);
     }
 
+
+
+    /**
+     * @param i
+     * The index of the scene we want to switch to.
+     * The following index are for :
+     * 0 -> SelectionView
+     * 1 -> LearningView
+     * 2 -> CreateView
+     */
     public void changeScene(int i) {
         stage.setScene(scenes.get(i));
     }
--- src/main/java/com/example/flashcards/controller/SelectionViewController.java
+++ src/main/java/com/example/flashcards/controller/SelectionViewController.java
@@ -1,9 +1,10 @@
 package com.example.flashcards.controller;
 
 import com.example.flashcards.command.ChangeSceneCommand;
+import com.example.flashcards.command.ExitCommand;
 import com.example.flashcards.models.DeckContainer;
 import com.example.flashcards.view.*;
-import javafx.application.Platform;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -14,24 +15,52 @@ public class SelectionViewController implements Observer, Initializable {
     private DeckContainer app;
     private ViewState viewState;
 
-    public SelectionViewController(DeckContainer app,ViewState ViewState){
+    /**
+     * @param app
+     * The DeckContainer that is used to control decks and cards.
+     *
+     * @param viewState
+     * The ViewState used to know at any time in which Scene we are.
+     */
+    public SelectionViewController(DeckContainer app,ViewState viewState){
         this.app=app;
-        this.viewState=ViewState;
+        this.viewState=viewState;
         app.addObserver(this);
         viewState.addObserver(this);
     }
 
-    public void initialize(URL location, ResourceBundle resources) {
 
-    }
-    public void update(){
 
-    }
+    /**
+     * Implementation of initialize method for Initializable interface :
+     *
+     * @param location
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resources
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
+    public void initialize(URL location, ResourceBundle resources) {}
+
+
+
+    /**
+     * Implementation of update() method for Observer interface
+     */
+    public void update(){}
+
+
+
+    /*
+     * Implementation of different methods for the pattern Command
+     */
+
     @FXML
-    public void Leave(){
-        Platform.exit();
+    public void exitCmd() {
+        new ExitCommand().execute();
     }
-
 
     @FXML
     public void changeToLearnCmd() {
