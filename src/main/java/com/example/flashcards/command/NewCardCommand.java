473e0252b2f676421f5f47b379032d09edc1edb7
Martin <martin.guitteny@telecomnancy.eu>
2023-01-04 09:21:12 +0100

    modif de NewCardCommand

--- src/main/java/com/example/flashcards/command/NewCardCommand.java
+++ src/main/java/com/example/flashcards/command/NewCardCommand.java
@@ -5,25 +5,11 @@ import com.example.flashcards.models.Deck;
 import com.example.flashcards.models.DeckContainer;
 
 public class NewCardCommand implements Command{
-    private String question;
-    private String answer;
-    private Deck deck;
-    Boolean twoSided;
     private DeckContainer deckContainer;
-    public NewCardCommand(Deck deck, DeckContainer deckContainer, String question, String answer,Boolean twoSided){
-
-        this.deck=deck;
+    public NewCardCommand(DeckContainer deckContainer){
         this.deckContainer=deckContainer;
-        this.question=question;
-        this.answer=answer;
-        this.twoSided=twoSided;
     }
     public void execute(){
-        Card card=new Card();
-        card.setTwoSided(twoSided);
-        card.setQuestion(question);
-        card.setAnswer(answer);
-        deck.addCard(card);
-        deckContainer.addCard(card);
+        deckContainer.newCard();
     }
 }
