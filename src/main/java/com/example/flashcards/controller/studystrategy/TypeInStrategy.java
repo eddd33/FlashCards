16caea039b993f503d425ca4283053f54fb2a752
PETIT Thomas <thomas.petit@telecomnancy.eu>
2023-01-06 17:00:37 +0100

    restrictions difficulté

--- src/main/java/com/example/flashcards/controller/studystrategy/TypeInStrategy.java
+++ src/main/java/com/example/flashcards/controller/studystrategy/TypeInStrategy.java
@@ -10,7 +10,14 @@ public class TypeInStrategy implements StudyStrategy{
     public void nextCard(double coef, Study study) {
         if (study.getStudyList().size() >0) {
             Card firstCard = study.getStudyList().get(0);
-            firstCard.setDifficulty(firstCard.getDifficulty() * coef);
+            if (firstCard.getDifficulty() * coef > 2) {
+                firstCard.setDifficulty(2);
+            } else if (firstCard.getDifficulty() * coef < 0.8) {
+                firstCard.setDifficulty(0.8);
+            }
+            else {
+                firstCard.setDifficulty(firstCard.getDifficulty() * coef);
+            }
             study.getApp().getCard(firstCard.getQuestion()).setDifficulty(firstCard.getDifficulty());
             study.getStudyList().remove(0);
             if (firstCard.getDifficulty() >= 1) {
